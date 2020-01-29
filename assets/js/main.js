8d23d37d865d4f3db629f191211288ca49adc10e
Vishal Rana <vishal.ch.36@gmail.com>
2020-01-30 03:19:07 +0530

    a bit refinement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,7 @@
 $(function() {
-  function setHeight() {
-    let height = $("#comment_form").height();
-    $("#comment_status").height(height);
-  }
   setHeight();
-  function setStyle() {
-    $(".comment-area").each(function() {
-      let sentiment = $(this).attr("sentiment");
-      switch (sentiment) {
-        case "bad":
-          $(this).css({
-            backgroundColor: "rgba(200,0,0,0.3)",
-            borderColor: "rgba(255,0,0,1)"
-          });
-          $(this)
-            .children(".sentiments")
-            .css({
-              backgroundColor: "rgba(200,0,0,0.1)"
-            });
-          break;
-        case "good":
-          $(this).css({
-            backgroundColor: "rgba(0,200,0,0.3)",
-            borderColor: "rgba(0,200,0,1)"
-          });
-          $(this)
-            .children(".sentiments")
-            .css({
-              backgroundColor: "rgba(0,200,0,0.1)"
-            });
-          break;
-          default:$(this).css({
-            backgroundColor: "rgba(0,0,200,0.3)",
-            borderColor: "rgba(0,0,200,1)"
-          });
-          $(this)
-            .children(".sentiments")
-            .css({
-              backgroundColor: "rgba(0,0,200,0.1)"
-            });
-          break;;
-      }
-    });
-  }
   setStyle();
-  $(".comment-area").click(function() {
+  $("#prev_comments").on("click",".comment-area",function() {
     $(this)
       .children(".sentiments")
       .slideToggle();
@@ -53,7 +10,7 @@ $(function() {
   $("#analyze_btn").click(function(event) {
     let data = $("#input").val();
     if (data != "") {
-      let result = "bad";
+      let result = getSentiment();
       let commentArea = document.createElement("div");
       let comment = document.createElement("div");
       let sentiments = document.createElement("div");
@@ -64,48 +21,106 @@ $(function() {
       comment.innerHTML = data;
       sentiments.innerHTML = result;
       commentArea.append(comment, sentiments);
-      $("#prev_comments").append(commentArea);
+      $("#prev_comments").prepend(commentArea);
       setStyle();
     }
     event.preventDefault();
   });
 
-  function createChart() {
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var myChart = new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: ["Good", "Bad", "Undetermined"],
-        datasets: [
-          {
-            label: "# of Sentimental Responses",
-            data: [5347,1244,1391],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.6)",
-              "rgba(54, 162, 235, 0.6)",
-              "rgba(255, 206, 86, 0.6)"
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)"
-            ],
-            borderWidth: 1
-          }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true
-              }
-            }
-          ]
-        }
-      }
-    });
-  }
   createChart();
 });
+function getSentiment() {
+  let val = Math.floor(Math.random() * 3);
+  switch (val) {
+    case 0:
+      return "bad";
+      break;
+    case 1:
+      return "good";
+      break;
+    default:
+      return "indeterminate";
+      break;
+  }
+}
+function setHeight() {
+  let height = $("#comment_form").height();
+  $("#comment_status").height(height);
+}
+function setStyle() {
+  $(".comment-area").each(function() {
+    let sentiment = $(this).attr("sentiment");
+    switch (sentiment) {
+      case "bad":
+        $(this).css({
+          backgroundColor: "rgba(200,0,0,0.3)",
+          borderColor: "rgba(255,0,0,1)"
+        });
+        $(this)
+          .children(".sentiments")
+          .css({
+            backgroundColor: "rgba(200,0,0,0.1)"
+          });
+        break;
+      case "good":
+        $(this).css({
+          backgroundColor: "rgba(0,200,0,0.3)",
+          borderColor: "rgba(0,200,0,1)"
+        });
+        $(this)
+          .children(".sentiments")
+          .css({
+            backgroundColor: "rgba(0,200,0,0.1)"
+          });
+        break;
+      default:
+        $(this).css({
+          backgroundColor: "rgba(0,0,200,0.3)",
+          borderColor: "rgba(0,0,200,1)"
+        });
+        $(this)
+          .children(".sentiments")
+          .css({
+            backgroundColor: "rgba(0,0,200,0.1)"
+          });
+        break;
+    }
+  });
+}
+function createChart() {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Good", "Bad", "Undetermined"],
+      datasets: [
+        {
+          label: "# of Sentimental Responses",
+          data: [5347, 1244, 1391],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.6)",
+            "rgba(54, 162, 235, 0.6)",
+            "rgba(255, 206, 86, 0.6)"
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)"
+          ],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+}
