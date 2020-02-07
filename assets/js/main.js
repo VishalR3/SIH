7359eb685a49328a3edde79bdd4c078d805b5797
Vishal Rana <vishal.ch.36@gmail.com>
2020-02-07 19:11:57 +0530

    Changes Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,7 @@ $(function() {
       setStyle();
     }
     event.preventDefault();
+    $("#input").val("");
   });
 
   createChart();
@@ -123,14 +124,14 @@ function createChart(good=5347,bad=1244,other=1391) {
           label: "# of Sentimental Responses",
           data: [good,bad,other],
           backgroundColor: [
-            "rgba(255, 99, 132, 0.6)",
-            "rgba(54, 162, 235, 0.6)",
-            "rgba(255, 206, 86, 0.6)"
+            "rgba(0, 255, 0, 0.6)",
+            "rgba(255, 0, 0, 0.6)",
+            "rgba(0, 0, 255, 0.6)"
           ],
           borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)"
+            "rgba(0, 255, 0, 1)",
+            "rgba(255, 0, 0, 1)",
+            "rgba(0, 0, 255, 1)"
           ],
           borderWidth: 1
         }
